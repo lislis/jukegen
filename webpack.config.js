07ec2541f273e5b27d9b1878dfbcfe4815eafafc
lislis <mail@lislis.de>
2018-09-06 19:00:14 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,20 @@ module.exports = {
     library: 'jukeGen',
     libraryTarget: 'umd'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   devServer: {
     contentBase: './dist'
   },
